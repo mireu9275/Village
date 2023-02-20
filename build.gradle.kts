ab5f39554f5afbaaab6f6ee0ec65a6799b10f939
mireu9275 <mireu9275@naver.com>
2023-02-20 10:26:52 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
 }
 
 group = "kr.hjkim.village"
@@ -9,16 +8,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("com.zaxxer:HikariCP:4.0.3")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    compileOnly("kr.hjkim.library:KimLibrary:1.0")
 }
---
+++ src/main/kotlin/Village.kt
@@ -0,0 +1,12 @@
+import org.bukkit.plugin.java.JavaPlugin
+
+class Village: JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+}
