b444be5190d1803137937fd3f81f9b56bc9e1730
mireu9275 <mireu9275@naver.com>
2023-02-20 11:01:24 +0900

    Updates

--- src/main/kotlin/objects/Village.kt
+++ src/main/kotlin/objects/Village.kt
@@ -1,7 +1,8 @@
 package objects
 
 import org.bukkit.entity.Player
+import java.util.UUID
 
 class Village(val player: Player) {
-    private val playerMap = HashSet<VillagePlayer>()
+    private val playerMap = HashMap<UUID,VillagePlayer>()
 }
