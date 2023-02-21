c30c5e3a055b554968aa38f77905be39c996c582
mireu9275 <mireu9275@naver.com>
2023-02-21 14:20:55 +0900

    update

--- src/main/kotlin/kr/hjkim/village/Village.kt
+++ src/main/kotlin/kr/hjkim/village/Village.kt
@@ -1,15 +1,15 @@
 package kr.hjkim.village
 
 import kr.hjkim.library.KimPlugin
+import kr.hjkim.library.managers.DataBaseManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class Village: KimPlugin() {
     override fun onEnable() {
-        super.onEnable()
+        DataBaseManager.registerSQL()
     }
 
     override fun onDisable() {
-        super.onDisable()
+        DataBaseManager.unRegisterSQL()
     }
-
 }
